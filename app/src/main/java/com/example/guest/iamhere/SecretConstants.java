7107da3b4d62eafb9c352014d91fdcb0b51a2a3c
Mark <mark.aaron.fisher@gmail.com>
2017-06-20 08:18:12 -0700

    troubleshoot the color palette variable name changes and create SecretConstants class that may or may not be useful in the future

---
+++ app/src/main/java/com/example/guest/iamhere/SecretConstants.java
@@ -0,0 +1,9 @@
+package com.example.guest.iamhere;
+
+/**
+ * Created by mf on 6/20/17.
+ */
+
+public class SecretConstants {
+    public static final String googleApiKey = "AIzaSyDfMV7ER0MExgrl4eFyOTTcKXxBxHPJoZU";
+}
