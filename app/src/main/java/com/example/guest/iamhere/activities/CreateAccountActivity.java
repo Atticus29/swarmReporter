257a0dd9db29df7aa2d6627b60015bf8d7b7ec54
Mark <mark.aaron.fisher@gmail.com>
2017-06-20 09:40:37 -0700

    add custom button styling and modify the manifest to establish hierarchical relationships and a launch activity

--- app/src/main/java/com/example/guest/iamhere/activities/CreateAccountActivity.java
+++ app/src/main/java/com/example/guest/iamhere/activities/CreateAccountActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import com.example.guest.iamhere.R;
 
+import butterknife.ButterKnife;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/example/guest/iamhere/activities/LoginGateActivity.java
+++ app/src/main/java/com/example/guest/iamhere/activities/LoginGateActivity.java
@@ -2,14 +2,23 @@ package com.example.guest.iamhere.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.guest.iamhere.R;
 
-public class LoginGateActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+
+public class LoginGateActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_gate);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
