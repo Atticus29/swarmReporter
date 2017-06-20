1a7a7257d8af27c64f5aac5131888b2d9503e404
Mark <mark.aaron.fisher@gmail.com>
2017-06-20 15:53:30 -0700

    get city name from the newSwarmReportActivity

--- app/src/main/java/com/example/guest/iamhere/activities/NewSwarmReportActivity.java
+++ app/src/main/java/com/example/guest/iamhere/activities/NewSwarmReportActivity.java
@@ -1,6 +1,14 @@
 package com.example.guest.iamhere.activities;
 
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,18 +18,29 @@ import android.widget.RadioButton;
 
 import com.example.guest.iamhere.R;
 import com.example.guest.iamhere.models.SwarmReport;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class NewSwarmReportActivity extends AppCompatActivity implements View.OnClickListener {
+public class NewSwarmReportActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private String TAG = NewSwarmReportActivity.class.getSimpleName();
     private FirebaseDatabase database;
     private DatabaseReference ref;
@@ -29,6 +48,11 @@ public class NewSwarmReportActivity extends AppCompatActivity implements View.On
     private String accessibility;
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
+    private GoogleApiClient mGoogleApiClient;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationRequest mLocationRequest;
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+    private String city;
 
     @Bind(R.id.reportSwarmButton) Button reportSwarmButton;
     @Bind(R.id.baseball) RadioButton baseball;
@@ -60,13 +84,96 @@ public class NewSwarmReportActivity extends AppCompatActivity implements View.On
                 }
             }
         };
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
 
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location == null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }else {
+            handleNewLocation(location);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String permissions[], int[] grantResults){
+        switch(requestCode){
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                    if(location == null){
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                    }else {
+                        handleNewLocation(location);
+                    }
+                }
+            }
+        }
+    }
+
+    private void handleNewLocation(Location location){
+        Log.d("hi", location.toString());
+        double currenLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currenLatitude, currentLongitude);
+        Geocoder gcd = new Geocoder(NewSwarmReportActivity.this, Locale.getDefault());
+        try{
+            List<Address> addresses = gcd.getFromLocation(currenLatitude, currentLongitude, 1);
+            if (addresses.size() > 0)
+            {
+                city = addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea() ;
+                Log.d(TAG, city);
+            }
+            else
+            {
+                city = "unknown";
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Location services suspended. Please reconnect");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if(connectionResult.hasResolution()){
+            try{
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch(IntentSender.SendIntentException e){
+                e.printStackTrace();
+            }
+        } else{
+            Log.d(TAG, "Location services failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
 
     @Override
     public void onStart() {
         super.onStart();
         auth.addAuthStateListener(authListener);
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -74,6 +181,10 @@ public class NewSwarmReportActivity extends AppCompatActivity implements View.On
         super.onStop();
         if (authListener != null) {
             auth.removeAuthStateListener(authListener);
+        }
+        if(mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
         }
     }
 
@@ -90,6 +201,7 @@ public class NewSwarmReportActivity extends AppCompatActivity implements View.On
             java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
             String timeString = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(currentTimestamp);
             Log.d(TAG, timeString);
+            Log.d("city in onClick", city);
 //            SwarmReport newSwarmReport(location, city, userName, userUid, size, timeString, accessibility);
             database = FirebaseDatabase.getInstance();
 //            ref = database.getReference(city);
@@ -125,5 +237,10 @@ public class NewSwarmReportActivity extends AppCompatActivity implements View.On
             accessibility = "tallLadder";
         }
         return accessibility;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
