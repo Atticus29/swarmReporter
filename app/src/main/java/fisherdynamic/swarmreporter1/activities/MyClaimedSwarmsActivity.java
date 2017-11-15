ab9059e36c620710d469efc51d0b5cc8c0aeeb44
Mark <mark.aaron.fisher@gmail.com>
2017-11-15 11:09:21 -0800

    add code to implement a nav drawer in my claimed swarms activity

--- app/src/main/java/fisherdynamic/swarmreporter1/activities/MyClaimedSwarmsActivity.java
+++ app/src/main/java/fisherdynamic/swarmreporter1/activities/MyClaimedSwarmsActivity.java
@@ -1,8 +1,10 @@
 package fisherdynamic.swarmreporter1.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,12 +14,15 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import fisherdynamic.swarmreporter1.R;
 import fisherdynamic.swarmreporter1.models.SwarmReport;
 import fisherdynamic.swarmreporter1.viewHolders.FirebaseClaimViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
@@ -26,7 +31,7 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MyClaimedSwarmsActivity extends AppCompatActivity {
+public class MyClaimedSwarmsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Query swarmReportQuery;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
     private SharedPreferences mSharedPreferences;
@@ -63,7 +68,7 @@ public class MyClaimedSwarmsActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         hView = navigationView.getHeaderView(0);
 
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ArrayList<String> children = new ArrayList<>();
         children.add("users");
@@ -107,6 +112,71 @@ public class MyClaimedSwarmsActivity extends AppCompatActivity {
         myClaimedSwarmsRecyclerView.setAdapter(mFirebaseAdapter);
         myClaimedSwarmsRecyclerView.setVisibility(View.VISIBLE);
         Log.d(TAG, "setUpBlankAdapter done with function");
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+
+        if (id == R.id.action_newReport) {
+            Intent intent = new Intent(MyClaimedSwarmsActivity.this, NewSwarmReportActivity.class);
+            if (userName != null && userId != null) {
+                intent.putExtra("userName", userName);
+                intent.putExtra("userId", userId);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Unable to retrieve username and id", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        if (id == R.id.action_myClaims) {
+            Intent intent = new Intent(MyClaimedSwarmsActivity.this, MyClaimedSwarmsActivity.class);
+            if (userName != null && userId != null) {
+                intent.putExtra("userName", userName);
+                intent.putExtra("userId", userId);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Unable to retrieve username and id", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (id == R.id.action_myReportedSwarms) {
+            Intent intent = new Intent(MyClaimedSwarmsActivity.this, MyReportedSwarmsActivity.class);
+            if (userName != null && userId != null) {
+                intent.putExtra("userName", userName);
+                intent.putExtra("userId", userId);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Unable to retrieve username and id", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        addToSharedPreferences("userName", "");
+        addToSharedPreferences("userId", "");
+        addToSharedPreferences("photoUrl", "");
+        Intent intent = new Intent(fisherdynamic.swarmreporter1.activities.MyClaimedSwarmsActivity.this, LoginGateActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void addToSharedPreferences(String key, String passedUserName) {
+        mEditor.putString(key, passedUserName).apply();
     }
 
 }
